a1f5e1b21b6f43a32f59d1d4fbd115fffc097096
Joey Chen <1462492941@qq.com>
2026-09-05 16:32:16 +0800

    tmp project delete

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -54,6 +54,18 @@ export default defineConfig(({ mode }) => {
 
 
       define: {
+
+
+        __VUE_I18N_FULL_INSTALL__: true,
+
+
+        __VUE_I18N_LEGACY_API__: true,
+
+
+        __INTLIFY_JIT_COMPILATION__: true,
+
+
+        __INTLIFY_DROP_MESSAGE_COMPILER__: false,
         "process.env": JSON.stringify({
           VUE_APP_SENTRY_DNS:
             env.VITE_SENTRY_DSN ||
